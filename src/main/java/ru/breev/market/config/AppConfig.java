04af553f64a183135760d73eed9526e09f7860a0
e.m.breev <e.m.breev@gmail.com>
2019-11-30 23:16:32 +0300

    update project

--- src/main/java/ru/breev/market/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.breev.market.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(value = "ru.breev.market")
-public class AppConfig {
-}
