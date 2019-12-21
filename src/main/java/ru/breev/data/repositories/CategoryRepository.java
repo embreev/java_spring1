bf483d3068a23376af050754780bf64e09b7f1e3
e.m.breev <e.m.breev@gmail.com>
2019-12-21 21:21:56 +0300

    +

--- src/main/java/ru/breev/data/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.breev.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.breev.data.entities.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
